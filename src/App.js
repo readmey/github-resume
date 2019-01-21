ee977b15a9f6ed09bb2879dde636d3ae3e4bb722
readmey <mytrang.hong@hotmail.de>
2019-01-21 08:40:14 +0100

    add routes and pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LandingPage from './pages/LandingPage';
+import Resume from './pages/Resume'
+import "./App.css";
+require('dotenv').config()
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div id="app">
+      <Route exact path="/" component={LandingPage} />
+      <Route exact path="/user/:username/resume" component={Resume} />
+    </div>
+  </Router>
+);
+
 
 export default App;
