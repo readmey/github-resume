ac51b15848abdbb6c9916b7feb1cac9bf75b84e4
readmey <mytrang.hong@hotmail.de>
2019-01-23 18:17:20 +0100

    add test

--- src/pages/LandingPage.test.js
+++ src/pages/LandingPage.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import LandingPage from './LandingPage.js';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<LandingPage />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
 
 it('should update the state.username on input change', () => {
   const wrapper = shallow(<LandingPage />);
   wrapper.find('#username').simulate('change', {target: { value: 'test' }});
   expect(wrapper.state('username')).toBe('test');
 });
+
+it('renders error text "username required" when submit form with empty username', () => {
+  const wrapper = shallow(<LandingPage />);
+  wrapper.find('.form__input--submit').simulate('click', {preventDefault: () => {}});
+  expect(wrapper.exists('.input__validation--error')).toEqual(true)
+})
+
+it('should update the state.validationError when submit form with empty username', () => {
+  const wrapper = shallow(<LandingPage />);
+  wrapper.find('#username').simulate('change', { target: { value: ' ' }});
+  wrapper.find('.form__input--submit').simulate('click', {preventDefault: () => {}});
+  expect(wrapper.state('validationError')).toBe(true)
+})
--- src/pages/Resume.test.js
+++ src/pages/Resume.test.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import Resume from './Resume.js';
 
-it('should render the text Loading...  if state.data is null', () => {
-  const match = {
-    match : {
-      params: {
-        username: "test"
-      }
+const match = {
+  match : {
+    params: {
+      username: "test"
     }
   }
+}
+
+it('should render the text Loading...  if state.data is null', () => {
   const wrapper = shallow(<Resume {...match} />);
   expect(wrapper.text()).toBe('Loading...');
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Resume {...match} />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
