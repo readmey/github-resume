e4754b27c92de14d98ac3d88399e0e4ab0bb697d
My Trang Hong <mytrang.hong@prodyna.com>
2020-05-17 13:59:43 +0200

    fix routing for github-pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import LandingPage from './pages/LandingPage';
-import Resume from './pages/Resume'
-import NotFoundPage from './components/NotFoundPage'
+import LandingPage from "./pages/LandingPage";
+import Resume from "./pages/Resume";
+import NotFoundPage from "./components/NotFoundPage";
 
 import "./App.css";
 
 const App = () => (
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <div id="app">
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route path="/user/:username/resume" component={Resume} />
-        <Route component={NotFoundPage} />
-      </Switch>  
+        <Route path="/404-not-found" component={NotFoundPage} />
+      </Switch>
     </div>
   </Router>
 );
-
 
 export default App;
