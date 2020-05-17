e0437319a1f5e238e4096abb35af2c8acfce166e
readmey <mytrang.hong@hotmail.de>
2020-05-17 13:59:43 +0200

    fix routing for github-pages

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import Proptypes from 'prop-types'
+import React from "react";
+import Proptypes from "prop-types";
 
 const NotFoundPage = (props) => {
-  const { location } = props
+  const { location } = props;
 
-  setTimeout(() => {props.history.push('/')}, 1000)
+  setTimeout(() => {
+    props.history.push("/");
+  }, 1000);
 
   return (
     <div id="not-found">
       <div className="container container--bg-white">
         <div className="text--center ">
           <i className="far fa-meh fa-4x"></i>
-          <h3>404 - {location.state.error} </h3>
+          <h3>
+            404 - {location && location.state ? location.state.error : ""}
+          </h3>
           <p></p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 NotFoundPage.propTypes = {
-  location: Proptypes.object.isRequired
-}
+  location: Proptypes.object.isRequired,
+};
 
-export default NotFoundPage
+export default NotFoundPage;
