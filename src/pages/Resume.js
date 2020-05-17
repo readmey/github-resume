2ba5a9df44d43e0f894a15c29f32ea05bebe519f
My Trang Hong <mytrang.hong@prodyna.com>
2020-05-17 14:00:14 +0200

    fix styles and formatting

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,87 +1,86 @@
-import React, { Component } from 'react'
-import UserProfile from '../components/UserProfile'
-import Loader from '../components/Loader';
+import React, { Component } from "react";
+import UserProfile from "../components/UserProfile";
+import Loader from "../components/Loader";
 
-const GITHUB_API_USER = 'https://api.github.com/users/';
+const GITHUB_API_USER = "https://api.github.com/users/";
 
 export default class Resume extends Component {
   state = {
     data: null,
-    isFetching: false
-  }
+    isFetching: false,
+  };
 
   async componentDidMount() {
-    const { username } = this.props.match.params
-    this.setState({ isFetching: true })
+    const { username } = this.props.match.params;
+    this.setState({ isFetching: true });
 
     try {
-      const userData = await this.fetchUserData(username)
-      const userRepos = await this.fetchUserRepos(username)
+      const userData = await this.fetchUserData(username);
+      const userRepos = await this.fetchUserRepos(username);
       this.setState({
-        isFetching: false, 
-        data: { ...userData, repositories: userRepos }
-      })
-    } catch(err) {
+        isFetching: false,
+        data: { ...userData, repositories: userRepos },
+      });
+    } catch (err) {
       this.props.history.push({
-        pathname: '/404',
-        state: { 
+        pathname: "/404-not-found",
+        state: {
           user: this.props.match.params.username,
-          error: err.message
-        }
-      })
+          error: err.message,
+        },
+      });
     }
   }
 
   handleErrors(response) {
     if (!response.ok) {
-      var error = new Error(response.statusText || response.status)
-      return Promise.reject(error)
+      var error = new Error(response.statusText || response.status);
+      return Promise.reject(error);
     }
     return response.json();
-}
-  
-  fetchUserData(username) {
-      return fetch(GITHUB_API_USER + username)
-      .then(this.handleErrors)
   }
-  
+
+  fetchUserData(username) {
+    return fetch(GITHUB_API_USER + username).then(this.handleErrors);
+  }
+
   fetchUserRepos(username) {
-    return fetch(GITHUB_API_USER + username + '/repos')
+    return fetch(GITHUB_API_USER + username + "/repos")
       .then(this.handleErrors)
-      .then(repositories => this.fetchReposLanguages(repositories))
+      .then((repositories) => this.fetchReposLanguages(repositories));
   }
 
   fetchReposLanguages(repositories) {
-    return Promise.all(repositories.map(repo =>
-      fetch(repo.languages_url)
-      .then(this.handleErrors)
-      .then(repoLanguages => {
-        return {
-          name: repo.name,
-          description: repo.description,
-          url: repo.html_url,
-          languages: repoLanguages,
-          stars: repo.stargazers_count,
-          watchers: repo.watchers_count
-        }
-      })
-    ))
+    return Promise.all(
+      repositories.map((repo) =>
+        fetch(repo.languages_url)
+          .then(this.handleErrors)
+          .then((repoLanguages) => {
+            return {
+              name: repo.name,
+              description: repo.description,
+              url: repo.html_url,
+              languages: repoLanguages,
+              stars: repo.stargazers_count,
+              watchers: repo.watchers_count,
+            };
+          })
+      )
+    );
   }
 
-  render () {
-    const { username } = this.props.match.params
-    const { data, isFetching } = this.state
+  render() {
+    const { username } = this.props.match.params;
+    const { data, isFetching } = this.state;
 
-    if(!isFetching && data) {
+    if (!isFetching && data) {
       return (
         <div id="resume">
           <UserProfile data={data} username={username} />
         </div>
-      )
+      );
     } else {
-      return (
-        <Loader />
-      )
+      return <Loader />;
     }
   }
 }
