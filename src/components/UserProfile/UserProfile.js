982fe55f4dbd785ee67ca2fd91016bc5de5944e9
readmey <mytrang.hong@hotmail.de>
2020-05-17 14:00:14 +0200

    fix styles and formatting

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
-import Card from '../Card'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Card from "../Card";
 
-import './UserProfile.css'
+import "./UserProfile.css";
 
 const UserProfile = (props) => {
-  const { username } = props
-  const {name, bio, location, company, login, avatar_url, repositories} = props.data
+  const { username } = props;
+  const {
+    name,
+    bio,
+    location,
+    company,
+    login,
+    avatar_url,
+    repositories,
+  } = props.data;
 
   return (
     <div className="container container--bg-white">
@@ -15,34 +23,56 @@ const UserProfile = (props) => {
         <i className="icon fas fa-chevron-left fa-2x"></i>
       </Link>
       <div className="block--flex">
-        <div className="block__item--flex">
+        <div className="block__item">
           <img className="avatar__img" src={avatar_url} alt="avatar" />
         </div>
-        <div className="block__item--flex">
-          {name ? (<p><i className="icon fas fa-user"></i> {name}</p>) : null}
-          {login ? <p><i className="icon fab fa-github"></i> {login}</p> : null}
-          {bio ? (<p><i className="icon fas fa-book"></i> {bio}</p>) : null}
-          {location ? (<p><i className="icon fas fa-map-marker-alt"></i>{location}</p>) : null}
-          {company ? (<p><i className="icon far fa-building"></i>{company}</p>) : null}
+        <div className="block__item">
+          {name ? (
+            <p>
+              <i className="icon fas fa-user"></i> {name}
+            </p>
+          ) : null}
+          {login ? (
+            <p>
+              <i className="icon fab fa-github"></i> {login}
+            </p>
+          ) : null}
+          {bio ? (
+            <p>
+              <i className="icon fas fa-book"></i> {bio}
+            </p>
+          ) : null}
+          {location ? (
+            <p>
+              <i className="icon fas fa-map-marker-alt"></i>
+              {location}
+            </p>
+          ) : null}
+          {company ? (
+            <p>
+              <i className="icon far fa-building"></i>
+              {company}
+            </p>
+          ) : null}
         </div>
       </div>
       <br />
       <div className="block--flex respositories">
-        { repositories.length > 0 ? repositories.map((repo, index) => (
-          <Card item={repo} key={index} />
-        )) : (
+        {repositories.length > 0 ? (
+          repositories.map((repo, index) => <Card item={repo} key={index} />)
+        ) : (
           <div className="info">
-            <i className="icon fas fa-info-circle"></i>               
+            <i className="icon fas fa-info-circle"></i>
             <span>{username} does not has any repositories</span>
           </div>
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 UserProfile.propTypes = {
-  data: PropTypes.object.isRequired
-}
+  data: PropTypes.object.isRequired,
+};
 
-export default UserProfile
+export default UserProfile;
